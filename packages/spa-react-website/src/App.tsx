fe5110f762734332f78cc970698f3812ef2f9332
jialin <linao264590000@gmail.com>
2025-04-05 14:37:45 +0800

    refactor: packages/spa-react-website init

---
+++ packages/spa-react-website/src/App.tsx
@@ -0,0 +1,14 @@
+//import { Button } from '../../shared-components/src/components/Button'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-3xl font-bold mb-4">SPA React Website</h1>
+      <div className="space-x-4">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
